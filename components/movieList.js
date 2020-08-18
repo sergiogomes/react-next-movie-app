b5f46b9f00977d6adfa30e7a3e92606262f0fee6
sergiogomes <sergiopgomes92@gmail.com>
2020-08-18 08:29:17 -0300

    Working with props

--- components/movieList.js
+++ components/movieList.js
@@ -1,42 +1,11 @@
 import React from "react";
 
 class MovieList extends React.Component {
-  state = {
-    count: 0,
-  };
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      count: 0,
-    };
-  }
-
-  increment = () => {
-    const { count } = this.state;
-    this.setState({
-      count: count + 1,
-    });
-  };
-
-  decrement = () => {
-    const { count } = this.state;
-    this.setState({ count: count - 1 });
-  };
-
   render() {
     return (
       <React.Fragment>
         <div>
-          <button onClick={this.increment} className="btn btn-primary">
-            Increment Number
-          </button>
-          <button onClick={this.decrement} className="btn btn-primary">
-            Decrement Number
-          </button>
-
-          <h1>{this.state.count}</h1>
+          <h1>{this.props.count}</h1>
         </div>
 
         <div className="col-lg-4 col-md-6 mb-4">
--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,7 +1,10 @@
-const SideMenu = () => {
+import { useState } from "react";
+
+const SideMenu = (props) => {
   return (
     <div>
-      <h1 className="my-4">Shop Name</h1>
+      <button onClick={props.clickHandler}>Click me</button>
+      <h1 className="my-4">{props.appName}</h1>
       <div className="list-group">
         <a href="#" className="list-group-item">
           Category 1
@@ -12,6 +15,9 @@ const SideMenu = () => {
         <a href="#" className="list-group-item">
           Category 3
         </a>
+      </div>
+      <div>
+        <h1>{props.count}</h1>
       </div>
     </div>
   );
